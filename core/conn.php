872ec68e31932e8e8623a3362a13a2cfb4d06e76
unknown <innovativeroh@gmail.com>
2024-10-20 14:44:19 +0530

    GamerLottry

---
+++ core/conn.php
@@ -0,0 +1,9 @@
+<?php
+// Database connection parameters
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "gamerlottry";
+
+// Create connection
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
